f005c6e6bbc2fa9faf6ca13569543f3edd9c7bc7
ndqm88 <Nicholas.D.Quintero@gmail.com>
2019-11-18 18:52:13 -0500

    created a table withing the model for Survey

--- db/migrate/20191118234815_create_surveys.rb
+++ db/migrate/20191118234815_create_surveys.rb
@@ -1,7 +1,9 @@
 class CreateSurveys < ActiveRecord::Migration[6.0]
   def change
     create_table :surveys do |t|
-
+      t.string :name, null: false, default: ""
+      t.boolean :is_active, null: false, default: false
+      
       t.timestamps
     end
   end
