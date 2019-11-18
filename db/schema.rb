f8748ee15a2beedc94ca78e3589d77ea72b34698
Emilio Izquierdo <emilio@Emilios-MacBook-Pro.local>
2019-11-18 14:51:53 -0500

    Set null to false for first name and last name in user profile. Added deafult value of True to notification opt in. Replace pry with hirb in Gem files for testing purposes.

--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_16_183659) do
+ActiveRecord::Schema.define(version: 2019_11_18_161354) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "profiles", force: :cascade do |t|
     t.bigint "user_id"
-    t.string "first_name"
-    t.string "last_name"
+    t.string "first_name", null: false
+    t.string "last_name", null: false
     t.string "photo_url"
     t.string "phone1"
     t.string "phone2"
-    t.boolean "notification_opt_in"
+    t.boolean "notification_opt_in", default: true
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_profiles_on_user_id"
