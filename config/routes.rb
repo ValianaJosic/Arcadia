01a6b2c0158b7aa6593e4a7a98277a62928623d1
Valiana <valiana.josic@gmail.com>
2019-11-21 15:51:15 -0500

    created contacts form

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
+  root 'hello_world#index'
   get 'hello_world', to: 'hello_world#index'
+  resources :contacts
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
