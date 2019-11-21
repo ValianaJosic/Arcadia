b5f2e630f18dac0679cbfcf7ba92051168bb2bb5
Pitney <PJules602@gmail.com>
2019-11-20 22:56:48 -0500

    Event Controller (New File Structure)

---
+++ app/controllers/event_controller.rb
@@ -0,0 +1,51 @@
+class EventController < ApplicationController
+
+        def index
+          @Event = Event.all
+        end
+        
+        def show
+          @Event = Event.find(params[:id])
+        end
+        
+        def new
+          @Event = Event.new
+        end
+        
+        def edit
+          @Event = Event.find(params[:id])
+        end
+        
+        def create
+          @Event = Event.new(Event_params)
+        
+          if @Event.save
+            redirect_to @Event
+          else
+            render 'new'
+          end
+        end
+        
+        def update
+          @Event = Event.find(params[:id])
+        
+          if @Event.update(Event_params)
+            redirect_to @Event
+          else
+            render 'edit'
+          end
+        end
+        
+        def destroy
+          @Event = Event.find(params[:id])
+          @Event.destroy
+        
+          redirect_to Events_path
+        end
+        
+        private
+          def Event_params
+            params.require(:Event).permit(:title, :text)
+          end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   get 'hello_world', to: 'hello_world#index'
   get 'event' , to: 'event#index'
+  get '/forms/event', to: 'event#new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
