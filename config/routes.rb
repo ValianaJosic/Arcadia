c3609f4fe439bf5f8ae378846614dd833bdec082
Pitney <PJules602@gmail.com>
2019-11-21 09:17:59 -0500

    Event Controller - Database Writing(Work In Progrss)

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   get 'hello_world', to: 'hello_world#index'
   # get 'event' , to: 'event#index'
-  get 'event', to: 'event#new'
+  get 'event/new', to: 'event#new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
