ff3a0e29aab2f7f78c210344504655d49e8dc66a
Pitney <PJules602@gmail.com>
2019-11-22 18:07:46 -0500

    Precription View Updated - List is now a reach component

--- app/controllers/prescriptions_controller.rb
+++ app/controllers/prescriptions_controller.rb
@@ -1,15 +1,26 @@
 class PrescriptionsController < ApplicationController
 
-  def index    
-    if current_user
-      @prescriptions = current_user.prescriptions
-    else
-      redirect_to user_session_path, notice: 'You are not logged in.'
+  def index  
+
+    respond_to do |format|
+      format.html
+      format.json do
+        prescriptions = current_user
+                  .prescriptions
+        render json: prescriptions 
+      end
     end
+
+    # if current_user
+    #   @prescriptions = current_user.prescriptions
+    # else
+    #   redirect_to user_session_path, notice: 'You are not logged in.'
+    # end
  end
   
   def create
-  Prescription.create(
+
+    @prescriptionadd = Prescription.create(
       user: current_user,
       brand_name: params[:brandName],
       generic_name: params[:genericName],
