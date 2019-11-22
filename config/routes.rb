642d8fcb36d1f0abb9a66b15f2747cb1d9608b5e
Pitney <PJules602@gmail.com>
2019-11-21 19:52:39 -0500

    Event MVC Refactor - (Complies & Runs)

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   post  '/prescriptions/add'  => 'prescriptions#create'
   post  '/addresponses'       =>  'addresponses#create'
 
-  resources :event
+  resources :events
   resources :contacts
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
