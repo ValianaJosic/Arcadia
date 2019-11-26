92c578c97a85890b84b0063afd8903c639bdb4df
Pitney <PJules602@gmail.com>
2019-11-26 11:41:05 -0500

    Main Calendar Refactor Begin

---
+++ app/javascript/bundles/Calendar/components/Calendar.jsx
@@ -0,0 +1,9 @@
+import React, {Component} from "react"
+import axios from "axios"
+
+export default class Calendar extends React.Component {
+    render(){
+        return ( <h1> This is the Calendar </h1> )
+    }
+
+}
