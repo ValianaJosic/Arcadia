541cfd045ea64938d9f2c0a3828bebd7aea71da1
Pitney <PJules602@gmail.com>
2019-11-26 13:45:39 -0500

    Material UI Calendar Added

--- app/javascript/bundles/Calendar/components/Calendar.jsx
+++ app/javascript/bundles/Calendar/components/Calendar.jsx
@@ -1,10 +1,24 @@
-import React, {Component} from "react"
+import React, { Component } from "react"
 import axios from "axios"
+import Calendar from 'material-ui/DatePicker/Calendar';
 
 export default class Calendar extends React.Component {
-    
-    render(){
-        return ( <h1> This is the Calendar </h1> )
+
+    render() {
+        return (
+
+            <Calendar
+                autoOk={false}
+                cancelLabel={false}
+                firstDayOfWeek={1}
+                onTouchTapDay={this.handleTouchTapDay}
+                mode={'portrait'}
+                open={true}
+                ref="main-calendar"
+            />
+
+
+        )
     }
 
 }
