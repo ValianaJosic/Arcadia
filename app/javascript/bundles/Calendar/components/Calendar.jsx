db62964f00c4dd3c8ff82b2bc2e32bb84ca49321
Pitney <PJules602@gmail.com>
2019-11-26 21:57:10 -0500

    Creating Restore-Point

--- app/javascript/bundles/Calendar/components/Calendar.jsx
+++ app/javascript/bundles/Calendar/components/Calendar.jsx
@@ -1,11 +1,10 @@
 import React, {Component} from "react"
 import axios from "axios"
-// import Calendar from 'material-ui/DatePicker/Calendar';
 
 export default class Calendar extends React.Component {
 
     render(){
-        return (<h1>Things</h1>)
+        return (<h1>CALENDAR</h1>)
     }
 
 }
