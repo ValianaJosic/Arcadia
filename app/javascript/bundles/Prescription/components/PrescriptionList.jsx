a2b48142167b403b6f105284160391659c6c5b61
Pitney <PJules602@gmail.com>
2019-11-27 19:43:34 -0500

    Precriptions List React Component Fixed

--- app/javascript/bundles/Prescription/components/PrescriptionList.jsx
+++ app/javascript/bundles/Prescription/components/PrescriptionList.jsx
@@ -7,28 +7,35 @@ export default class PrescriptionList extends React.Component {
     state = { DrugsList: [] }
 
     fetchDrugsList = () => {
-        DrugsList = this.state;
-            axios.get(`prescriptions.json`).then((response => {
+        axios.get(`prescriptions.json`).then((response => {
             const data = response.data
-            this.setState({ DrugsList: data })
-            }))
+            this.setState({ DrugsList: data }, () => console.log(this.state.DrugsList, ""))
+        }))
+    }
+
+    componentDidMount() {
+        this.fetchDrugsList()
     }
 
     render() {
-        let myObj = this.state.DrugsList
         return (
-            <p>look into fetch function</p>
-            // myObj.forEach(drug => {
-            //     <div>
-            //         <div>{drug.brand_name}</div>
-            //         <div>{drug.generic_name}</div>
-            //         <div>{drug.dosage_form}</div>
-            //         <div>{drug.product_type}</div>
-            //         <div>{drug.product_ndc}</div>
-            //         <div>{drug.user_dosage}</div>
-            //         <div>{drug.user_notes}</div>
-            //     </div>
-            // })
+            <div>
+                {this.state.DrugsList && this.state.DrugsList.map(drug => {
+                    return (
+                        <>
+                            <div>{drug.id}</div>
+                            <div>{drug.generic_name}</div>
+                            <div>{drug.dosage_form}</div>
+                            <div>{drug.product_type}</div>
+                            <div>{drug.product_ndc}</div>
+                            <div>{drug.user_dosage}</div>
+                            <div>{drug.user_notes}</div>
+                        </>
+                    )
+
+                })
+                }
+            </div>
         )
     }
 
