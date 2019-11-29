6f09e2a77ae495b71307e39baa41472deb803b65
Alexia Panameno <alexiapanameno@gmail.com>
2019-11-29 13:39:44 -0500

    agenda changes

--- app/controllers/agendas_controller.rb
+++ app/controllers/agendas_controller.rb
@@ -1,6 +1,9 @@
 class AgendasController < ApplicationController
 
   def index
+    @user = current_user
+    @prescriptions =  current_user.events.where(user: current_user, eventable_type: "Prescription").map { |prescription_event| prescription_event.eventable }
+    @contacts = @user.contacts
   end
 
 end
