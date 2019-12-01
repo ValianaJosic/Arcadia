d11e7411ded41809a2fd2c9b8e42671a14120d4d
Pitney <PJules602@gmail.com>
2019-11-30 21:30:40 -0500

    Agenda View wired up to backend

--- app/controllers/agendas_controller.rb
+++ app/controllers/agendas_controller.rb
@@ -3,7 +3,9 @@ class AgendasController < ApplicationController
   def index
     @user = current_user
     @prescriptions =  current_user.events.where(user: current_user, eventable_type: "Prescription").map { |prescription_event| prescription_event.eventable }
+    @appointments = current_user.events.where(user: current_user, eventable_type: "Contact").map { |contact_event| contact_event.eventable }
     @contacts = @user.contacts
+    @event = current_user.events.all
   end
 
 end
